f8f35cce98f84aa3e570fb6879ba0981b65b8901
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-11-19 01:50:34 -0500

    reset after upload

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-openocd -f interface/jlink.cfg -c "adapter_khz 2000; transport select swd; set WORKAREASIZE 0;" -f target/nrf51.cfg -c "init; reset; init; halt; nrf51 mass_erase; program $1; shutdown"
+openocd -f interface/jlink.cfg -c "adapter_khz 2000; transport select swd; set WORKAREASIZE 0;" -f target/nrf51.cfg -c "init; reset; init; halt; nrf51 mass_erase; program $1; reset; exit;"
